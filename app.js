7533903b7e3c9316bbc34c4b216973ba02dbd8d5
Byongchan Jeon <ckeis@naver.com>
2023-04-15 04:23:16 +0900

    src 정리

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const app = express();
 
 // View setting
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 // routing
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/", home);// 미들웨어 등록 메소드
 
 module.exports = app;
